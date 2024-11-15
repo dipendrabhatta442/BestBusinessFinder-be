5e7c3050a26407f88afc1ad8e513e7c26cd9efc9
dipendrabhatta442 <dipendrabhatta442@gmail.com>
2024-11-15 20:27:52 +0545

    Add files via upload
    init

---
+++ server.ts
@@ -0,0 +1,22 @@
+import express, { Application } from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import connectDB from './config/db';
+import authRoutes from './routes/authRoutes';
+import businessRoutes from './routes/businessRoutes';
+import authMiddleware from './middlewares/authMiddleware';
+
+dotenv.config();
+
+const app: Application = express();
+app.use(cors());
+app.use(express.json());
+connectDB();
+// Use the middleware for all routes that require API key validation=
+app.use(authMiddleware);
+
+app.use('/api/auth', authRoutes);
+app.use('/api/business', businessRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
